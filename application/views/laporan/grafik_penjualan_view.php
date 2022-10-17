a4aa86f2101830937b03e6919399fdd5a7c7eaaa
DominicusRA <dominicusrakaalfryann@gmail.com>
2022-10-18 04:18:56 +0700

    grafik

--- application/views/laporan/grafik_penjualan_view.php
+++ application/views/laporan/grafik_penjualan_view.php
@@ -370,17 +370,17 @@
                 pointHighlightStroke: 'rgba(60,141,188,1)',
                 data: <?= json_encode($var_jumlah) ?>
             },
-            {
-                label: 'Kardus Large',
-                backgroundColor: 'rgba(60,141,188,0.9)',
-                borderColor: 'rgba(60,141,188,0.8)',
-                pointRadius: false,
-                pointColor: '#3b8bba',
-                pointStrokeColor: 'rgba(60,141,188,1)',
-                pointHighlightFill: '#fff',
-                pointHighlightStroke: 'rgba(60,141,188,1)',
-                data: <?= json_encode($var_jumlah) ?>
-            }
+            // {
+            //     label: 'Kardus Large',
+            //     backgroundColor: 'rgba(60,141,188,0.9)',
+            //     borderColor: 'rgba(60,141,188,0.8)',
+            //     pointRadius: false,
+            //     pointColor: '#3b8bba',
+            //     pointStrokeColor: 'rgba(60,141,188,1)',
+            //     pointHighlightFill: '#fff',
+            //     pointHighlightStroke: 'rgba(60,141,188,1)',
+            //     data: <?= json_encode($var_jumlah) ?>
+            // }
         ]
 
         $(function() {
